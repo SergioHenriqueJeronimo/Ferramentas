4c03e99298212c2c4e12e5451aa89d9d132e3dd3
SergioHenriqueJeronimo <higoroiten@gmail.com>
2025-08-08 09:50:19 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -9,3 +9,27 @@ criaCartao(
     "O nome do herói que usa cueca por cima?",
     "O superman."
 )
+
+criaCartao(
+    "Anti-Herói",
+    "Anti-Herói muito piadista e com design igual do homem aranha",
+    "Deadpool."
+)
+
+criaCartao(
+    "Vilão",
+    "ele se veste de verde e faz parte de homem aranha",
+    "Duende Verde."
+)
+
+criaCartao(
+    "Heói",
+    "Conhecido por carregar o símbolo de sua nação no peito",
+    "Capitão América."
+)
+
+criaCartao(
+    "Anti-Herói",
+    "Conecido mais pelo semblante de morcego",
+    "Batman."
+)

daece9b0eb1d92c31d91c964201218b2c2630979
SergioHenriqueJeronimo <higoroiten@gmail.com>
2025-08-08 08:30:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,11 @@
+criaCartao(
+    "Vilões",
+    "Qual o nome do vilão que sorri muito?",
+    "o coringa."
+)
+
+criaCartao(
+    "Herói",
+    "O nome do herói que usa cueca por cima?",
+    "O superman."
+)
